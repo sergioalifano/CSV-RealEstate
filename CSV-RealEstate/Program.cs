8fcdf0fac5778bad97e350a67696687e44bb64ca
Sergio Alifano <sergio.alifano@gmail.com>
2015-02-11 20:54:25 -0700

    Constructor and populated list

--- CSV-RealEstate/Program.cs
+++ CSV-RealEstate/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
+using System.IO;
 
 namespace CSV_RealEstate
 {
@@ -19,6 +21,8 @@ namespace CSV_RealEstate
             
             //Display the average square footage of a Condo sold in the city of Sacramento, 
             //Use the GetAverageSquareFootageByRealEstateTypeAndCity() function.
+            Console.WriteLine(GetAverageSquareFootageByRealEstateTypeAndCity(realEstateSaleList, RealEstateType.Condo, "Sacramento")); 
+            
 
             //Display the total sales of all residential homes in Elk Grove.  Use the GetTotalSalesByRealEstateTypeAndCity() function for testing.
 
@@ -38,20 +42,37 @@ namespace CSV_RealEstate
             //Display top 5 cities by the number of homes sold (using the GroupBy extension)
             // Use the GetTop5CitiesByNumberOfHomesSold() function for testing.
 
+            Console.ReadKey();
+
         }
 
         public static List<RealEstateSale> GetRealEstateSaleList()
         {
-         
+
+            List<RealEstateSale> realEstateSaleList = new List<RealEstateSale>();
             //read in the realestatedata.csv file.  As you process each row, you'll add a new 
             // RealEstateData object to the list for each row of the document, excluding the first.  bool skipFirstLine = true;
+            StreamReader reader = new StreamReader("realestatedata.csv");
+
+            // Get and don't use the first line
+            string firstline = reader.ReadLine();
+
+            // Loop through the rest of the lines
+            while (!reader.EndOfStream)
+            {
+                string oneLineOfTextFromTheDocument = reader.ReadLine();
+                RealEstateSale estateLine = new RealEstateSale(oneLineOfTextFromTheDocument);
+                realEstateSaleList.Add(estateLine);
+            }
          
-            return new List<RealEstateSale>();
+            return realEstateSaleList;
         }
 
+        //Display the average square footage of a Condo sold in the city of Sacramento
         public static double GetAverageSquareFootageByRealEstateTypeAndCity(List<RealEstateSale> realEstateDataList, RealEstateType realEstateType, string city) 
         {
-            return 0.0;
+            return realEstateDataList.Where(x => x.City.ToLower() == city.ToLower() && x.Type==realEstateType).Average(x => x.Sq_Ft);
+             
         }
 
         public static decimal GetTotalSalesByRealEstateTypeAndCity(List<RealEstateSale> realEstateDataList, RealEstateType realEstateType, string city)
@@ -96,15 +117,64 @@ namespace CSV_RealEstate
     public enum RealEstateType
     {
         //fill in with enum types: Residential, MultiFamily, Condo, Lot
+        Residential,
+        MultiFamily,
+        Condo,
+        Lot
     }
     class RealEstateSale
     {
         //Create properties, using the correct data types (not all are strings) for all columns of the CSV
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Zip{get;set;}
+        public string State{get;set;}
+        public int Beds { get; set; }
+        public int Baths { get; set; }
+        public double Sq_Ft { get; set; }
+        public RealEstateType Type { get; set; }
+        public DateTime Sale_Date { get; set; }
+        public double Price { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+
 
         //The constructor will take a single string arguement.  This string will be one line of the real estate data.
         // Inside the constructor, you will seperate the values into their corrosponding properties, and do the necessary conversions
+        public RealEstateSale(string inputLine)
+        {
+            string[] myList = inputLine.Split(',');
 
-        //When computing the RealEstateType, if the square footage is 0, then it is of the Lot type, otherwise, use the string
-        // value of the "Type" column to determine its corresponding enumeration type.
+            this.Street = myList[0];
+            this.City = myList[1];
+            this.Zip = myList[2];
+            this.State = myList[3];
+            this.Beds = int.Parse(myList[4]);
+            this.Baths = int.Parse(myList[5]);
+            this.Sq_Ft = double.Parse(myList[6], CultureInfo.InvariantCulture.NumberFormat);
+
+            //When computing the RealEstateType, if the square footage is 0, then it is of the Lot type, otherwise, use the string
+            // value of the "Type" column to determine its corresponding enumeration type.
+            if (this.Sq_Ft == 0)
+            {
+                this.Type = (RealEstateType)Enum.Parse(typeof(RealEstateType), "Lot");
+            }
+            else if (myList[7] == "Multi-Family")
+            {
+                this.Type = (RealEstateType)Enum.Parse(typeof(RealEstateType), "MultiFamily");
+            }
+            else
+            { this.Type = (RealEstateType)Enum.Parse(typeof(RealEstateType), myList[7]); }
+
+            string format = "ddd MMM dd yyyy";
+            DateTime dateTime = DateTime.ParseExact(myList[8], format, CultureInfo.InvariantCulture);
+            this.Sale_Date = dateTime;
+
+            this.Price = double.Parse(myList[9], CultureInfo.InvariantCulture.NumberFormat);
+            this.Latitude = double.Parse(myList[10], CultureInfo.InvariantCulture.NumberFormat);
+            this.Longitude = double.Parse(myList[11], CultureInfo.InvariantCulture.NumberFormat);
+        }
+
+       
     }
 }
